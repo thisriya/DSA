8e9d54ab9870ee96bf6d86c8bd7954e424293b74
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-08-17 23:13:51 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
 //UNION OF SORTED ARRAY
 class solution{
     public:
@@ -91,3 +93,120 @@ vector<int> mergeSortedArrays(vector<int>& arr1, vector<int>& arr2) {
 
     return result;
 	}
+
+
+// REVERSE STRING USING STACK
+char* reverse(char *S, int len)
+{
+    stack<char> c;
+    for (int i=0;i<len;i++) {
+        c.push(S[i]);
+    }
+    for (int i=0;i<len;i++) {
+        S[i] = c.top();
+        c.pop();
+    }
+   return S;
+}
+
+
+//SWAP THE ARRAY ELEMENTS
+    void swapElements(int arr[], int n){
+        for (int i=0;i<n-2;i++) {
+        // Swap elements at index i and i + 2
+        int temp = arr[i];
+        arr[i] = arr[i + 2];
+        arr[i + 2] = temp;
+    }
+}
+
+//EQUILIBIRIUM POINT
+    int equilibriumPoint(vector<long long> &arr) {
+        long long sum=0;
+        if(arr.size()==1) return 1;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+        }
+        long long l_sum=0;
+        for(int i=0;i<arr.size();i++){
+            if(l_sum == sum - l_sum - arr[i]){
+                return i+1;
+            }
+            l_sum+=arr[i];
+        }
+        return -1;
+    }
+
+// CHECK PANAGRAM
+    bool checkPangram (string s) {
+        bool ans[26]={false};
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        for (char ch : s) {
+        if (isalpha(ch)) {
+            ans[ch - 'a'] = true;
+        }
+    }
+    for (bool b : ans) {
+        if (!b) {
+            return false;
+        }
+    }
+  return true; 
+}
+
+// NUMBER OF INTERSECTION IN 2 ARRAYS
+   int NumberofElementsInIntersection(int a[], int b[], int n, int m)
+    {
+    unordered_set<int> setA(a, a + n);
+    unordered_set<int> setB(b, b + m);
+    int c = 0;
+   for (int i=0;i<m; i++) {
+        if (setA.find(b[i]) != setA.end()) {
+            c++;
+            setA.erase(b[i]);  // Remove the element to avoid counting duplicates
+        }
+    }
+    return c;
+}
+
+// FIRST NON-REPEATING ELEMENT
+   int firstNonRepeating(vector<int>& arr) {
+        map<int,int> result;
+        for(auto num: arr){
+            result[num]++;
+        }
+        for(auto num: arr){
+            if(result[num]==1){
+                return num;
+            }
+        }
+        return 0;
+    }
+
+
+// COMMON IN 3 SORTED ARRAY
+    vector<int> commonElements(vector<int> &arr1, vector<int> &arr2,vector<int> &arr3) {
+        vector<int> r;
+        int i=0,j=0,k=0;
+        while(i<arr1.size() && j<arr2.size() && k<arr3.size()){
+            if(arr1[i]==arr2[j] && arr1[i]==arr3[k]){
+                 if (r.empty() || r.back() != arr1[i]) {
+                 r.push_back(arr1[i]);
+            }
+                i++;
+                j++;
+                k++;
+            }
+        
+            else if(arr1[i]<arr2[j])
+                i++;
+                
+            else if(arr2[j]<arr3[k])
+                j++;
+                
+            else
+                k++;
+            
+        }
+        return r;
+  }
