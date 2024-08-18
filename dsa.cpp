c285555611016ab8ea6ea0a33b65eea1f44f61c5
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-08-18 23:55:47 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -260,3 +260,32 @@ class Solution{
     
     }
 };
+
+// IDENTICAL TREES
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        if(r1==nullptr && r2==nullptr){
+            return true;
+        }
+        if(r1==nullptr || r2==nullptr){
+            return false;
+        }
+        
+        return (r1->data == r2->data) && isIdentical(r1->left,r2->left) && isIdentical(r1->right,r2->right);
+    }
+};
