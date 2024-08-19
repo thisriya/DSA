b3877fc720665cf03d2bd0cd2d6d989ce3cc2bfe
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-08-19 20:23:50 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -303,3 +303,25 @@ class Solution
         }
         return 1;
     }
+
+
+// IDENTICAL LINKED LIST USING NEXT 
+struct Node {
+  int data;
+  struct Node *next;
+
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+bool areIdentical(struct Node *head1, struct Node *head2) {
+  while(head1!=nullptr & head2!=nullptr){
+      if(head1->data!=head2->data){
+          return false;
+      }
+      head1=head1->next;
+      head2=head2->next;
+  }
+  return head1==nullptr && head2==nullptr;
+}
