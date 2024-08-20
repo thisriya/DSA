8bba4d6b7ea0ea84c2d7c396ae3589d1d38d2952
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-08-20 23:32:23 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -477,3 +477,26 @@ bool pairWiseConsecutive(stack<int> s)
    
    return ans;
 }
+
+
+// STACK DESIGNER
+stack<int>_push(int arr[],int n)
+{
+    stack<int> s;
+     for (int i=0;i<n;i++) {
+        s.push(arr[i]);
+    }
+    return s;
+}
+
+/* _pop function to print elements of the stack
+   remove as well
+*/
+void _pop(stack<int> s)
+{
+    
+    while(!s.empty()) {
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+}
