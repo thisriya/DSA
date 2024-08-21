6542348cba77c519354a9cdd80854627c9303175
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-08-21 23:53:31 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -520,3 +520,28 @@ class Solution {
         }
     }
 };
+
+
+// COUNT LENGTH OF LOOP
+    int countNodesinLoop(struct Node *head) {
+        if (!head) return 0;  // If the list is empty, return 0
+        
+        Node* slow = head;
+        Node* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                int count = 1;
+                Node* temp = slow;
+                
+                while (temp->next != slow) {
+                    count++;
+                    temp = temp->next;
+                }
+                
+                return count; 
+            }
+        }
+        return 0;
+    }
