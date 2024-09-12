f129a7ac2abcad3cacf35d9467eb4cda4ea4fbc7
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-09-13 00:33:27 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -823,3 +823,16 @@ def gfg(S):
         print ("Yes")
     else:
         print ("No")
+
+// FUNCTIONS
+def trim(str):
+    return str.strip()
+
+def exists(str, x):
+    return str.find(x)
+
+def titleIt(str):
+    return str.title()
+
+def casesSwap(str):
+    return str.swapcase()
