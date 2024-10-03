ad2a71b80e8884b9c01d0885ffb058753a0d6632
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-10-03 23:41:25 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -1386,3 +1386,23 @@ class Solution {
 
     return arr[0];
     }
+
+
+// MULTIPLY TWO LISTS
+#define MOD 1000000007
+class solution {
+  public:
+    long long multiplyTwoLists(Node *first, Node *second) {
+        long long num1=0,num2=0;
+        while(first!=nullptr){
+              num1=(num1*10+first->data)%MOD;
+              first=first->next;
+        }
+        
+         while(second!=nullptr){
+              num2=(num2*10+second->data)%MOD;
+              second=second->next;
+        }
+        
+        return (num1*num2)%MOD;
+    }
