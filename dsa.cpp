07de696db1686df9da5fb4cf8df19045238804f9
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-10-03 23:50:58 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -1406,3 +1406,31 @@ class solution {
         
         return (num1*num2)%MOD;
     }
+
+
+// INTERSECTION OF LINKED LIST
+class Solution {
+  public:
+    Node* findIntersection(Node* head1, Node* head2) {
+        Node* dummy=new Node(0);
+        Node* tail=dummy;
+        
+        while(head1!=nullptr && head2!=nullptr){
+            if(head1->data==head2->data){
+                tail->next=new Node(head1->data);
+                tail=tail->next;
+                head1=head1->next;
+                head2=head2->next;
+            }
+            
+            else if(head1->data<head2->data){
+                head1=head1->next;
+            }
+            
+            else{
+                head2=head2->next;
+            }
+        }
+        
+        return dummy->next;
+    }
