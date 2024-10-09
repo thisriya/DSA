473456f77ea66a2de06d1c2fc73475771474561f
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-10-09 19:37:53 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -1483,3 +1483,49 @@ class Solution {
      
        return result;
     }
+
+
+// MATRIX IN LINKED LIST
+struct Node
+{
+    int data;
+    Node* right, *down;
+
+    Node(int x){
+        data = x;
+        right = NULL;
+        down = NULL;
+    }
+};
+*/
+
+// function must return the pointer to the first element of the in linked list i.e. that
+// should be the element at arr[0][0]
+class Solution {
+  public:
+    Node* constructLinkedMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        if(n==0) {
+            return NULL;
+        }
+        
+        vector<vector<Node*>> nodes(n,vector<Node*>(n,NULL));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                nodes[i][j]=new Node(mat[i][j]);
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j<n-1){
+                    nodes[i][j]->right=nodes[i][j+1];
+                }
+                if(i<n-1){
+                    nodes[i][j]->down=nodes[i+1][j];
+                }
+            }
+        }
+        return nodes[0][0];   
+    }
+};
