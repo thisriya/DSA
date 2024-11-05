87e4eb8876005c8240ecd851d99a9a304f19c857
Riya Singh <125754667+thisriya@users.noreply.github.com>
2024-11-05 11:55:32 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -1782,3 +1782,16 @@ class Solution {
                 return "-1";
             }
         }
+
+
+// ROTATING MATRIX BY 90DEGREE (TAKE TRANSPOSE AND REVERSE THE ROWS)
+void rotate(vector<vector<int> >& mat) {
+    for(int i=0;i<mat.size();i++){
+        for(int j=i;j<mat.size();j++){
+            swap(mat[i][j],mat[j][i]);
+        }
+    }
+    for(int i=0;i<mat.size();i++){
+        reverse(mat[i].begin(),mat[i].end());
+    }
+}
